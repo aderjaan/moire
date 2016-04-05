188b474a45b808fddbe0a594cfb6ecec17802fe4
Jos van Velzen <jos.vanvelzen@changer.nl>
2016-04-05 12:36:05 +0200

    fix tsts

--- config/conf_test.go
+++ config/conf_test.go
@@ -8,7 +8,7 @@ import (
 func TestConfig(t *testing.T) {
 	gottp.MakeConfig(&Settings)
 
-	if Settings.Moire.DBAddress != "127.0.0.1:27017" {
-		t.Error("Improper Configuration")
+	if Settings.Moire.DbUrl != "mongodb://localhost/gallery" {
+		t.Error("Improper Configuration: " + Settings.Moire.DbUrl)
 	}
 }
